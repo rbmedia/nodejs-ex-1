33636009e066f918f77016d2ebaa9872439bada0
rbmedia <remiblot@gmail.com>
2017-02-04 00:54:48 +0100

    Update app_test.js

--- tests/app_test.js
+++ tests/app_test.js
@@ -18,6 +18,16 @@ describe('Basic routes tests', function() {
         })
 
     })
+    
+    it('GET to /test should return 200', function(done){
+        chai.request(reqServer)
+        .get('/')
+        .end(function(err, res) {
+            res.should.have.status(200);
+            done();
+        })
+
+    })
 
     it('GET to /pagecount should return 200', function(done){
         chai.request(reqServer)
